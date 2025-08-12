c82494151380e1ebfd5ccdab3276366c9c76d082
Sushil Nayak <sushil.nayak@gmail.com>
2025-08-12 22:52:45 +0530

    added storage

---
+++ lib/notes/storage.ex
@@ -0,0 +1,13 @@
+defmodule Notes.Storage do
+  def save_notes(notes, file) do
+    File.mkdir!(file)
+    File.write!(file, Jason.encode!(notes, pretty: true))
+  end
+
+  def get_notes(file) do
+    case File.read(file) do
+      {:ok, content} -> Jason.decode!(content)
+      {:error, :enoent} -> []
+    end
+  end
+end
